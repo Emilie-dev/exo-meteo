47c3d83bdee6c173c4e27eb26de9782515c29f43
Chauvin <chauvin-e@orange.fr>
2017-04-12 17:24:05 +0200

    try to add french language and metric unit

--- app.js
+++ app.js
@@ -2,23 +2,21 @@ $(document).ready(function() {
 
 	$.ajax({
 		type : "get",
-    	url :'http://api.openweathermap.org/data/2.5/weather?id=6448311&APPID=0dc029b5c62679926f9a47d2d19a06fc',
-       	
-       	success : function(data) {
-       		console.log(data);
-       		console.log('success');
-       	},
-       	error : function(err) {
-       		console.log(err);
+    	url :'http://api.openweathermap.org/data/2.5/weather?id=6448311&APPID=0dc029b5c62679926f9a47d2d19a06fc&mode=html&units=metric&lang={fr}'
+       	})
+
+		.done(function(data) {
+       		$("#afficher").append(data);
+       	})
+       		
+       	.fail(function() {
        		console.log('error');
-       	}
-    });
+       	})
 
-
-
-
-
-
+       	.always(function() {
+       		console.log('complete');
+       	});
+   
 
 
 
