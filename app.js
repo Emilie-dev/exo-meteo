2e20eb80a56209eef8d774891ec5e0af13d67835
Chauvin <chauvin-e@orange.fr>
2017-04-12 15:19:38 +0200

    add a url with ajax

--- app.js
+++ app.js
@@ -1,6 +1,18 @@
 $(document).ready(function() {
 
-
+	$.ajax({
+		type : "get",
+    	url :'http://api.openweathermap.org/data/2.5/weather?id=6448311&APPID=0dc029b5c62679926f9a47d2d19a06fc',
+       	
+       	success : function(data) {
+       		console.log(data);
+       		console.log('success');
+       	},
+       	error : function(err) {
+       		console.log(err);
+       		console.log('error');
+       	}
+    });
 
 
 
